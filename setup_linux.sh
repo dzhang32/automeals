13e04ae1740b6c4176291382e58af111111985ce
David Zhang <david@cosyne.com>
2025-07-08 12:06:33 +0100

    build: re-initialise repo

--- setup_linux.sh
+++
@@ -1,9 +0,0 @@
-# Vim
-sudo apt-get update
-sudo apt-get install vim
-
-# Starship
-curl -sS https://starship.rs/install.sh | sh
-
-# Atuin
-curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
