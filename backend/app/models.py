a991625ea4180286c5696f6339e0e1d3944b682a
David Zhang <32676710+dzhang32@users.noreply.github.com>
2025-07-09 10:24:40 +0100

    build: drop mypy

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,15 +4,25 @@ from sqlmodel import Field, Relationship, SQLModel
 
 
 class RecipeIngredientLink(SQLModel, table=True):
-    recipe_id: int | None = Field(default=None, foreign_key="recipe.id", primary_key=True)
-    ingredient_id: int | None = Field(default=None, foreign_key="ingredient.id", primary_key=True)
+    recipe_id: int | None = Field(
+        default=None, foreign_key="recipe.id", primary_key=True
+    )
+    ingredient_id: int | None = Field(
+        default=None, foreign_key="ingredient.id", primary_key=True
+    )
+
 
 class Recipe(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
-    ingredients: list["Ingredient"] = Relationship(back_populates="recipes", link_model=RecipeIngredientLink)
+    ingredients: list["Ingredient"] = Relationship(
+        back_populates="recipes", link_model=RecipeIngredientLink
+    )
+
 
 class Ingredient(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
-    recipes: list[Recipe] = Relationship(back_populates="ingredients", link_model=RecipeIngredientLink)
+    recipes: list[Recipe] = Relationship(
+        back_populates="ingredients", link_model=RecipeIngredientLink
+    )
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -2,16 +2,7 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-RECIPES_DB = {"potato_salad": ["test"]}
 
 @router.get("/")
 async def root():
     return {"message": "Hello World"}
-
-@router.get("/recipes")
-async def read_recipes() -> dict:
-    return RECIPES_DB
-
-@router.get("/ingredients/{recipe}")
-async def read_ingredients(recipe: str) -> list:
-    return RECIPES_DB.get(recipe)
