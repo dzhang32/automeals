dfa01916138bd48c2b1a120bbc1153fab1c6e731
David Zhang <32676710+dzhang32@users.noreply.github.com>
2025-07-28 21:30:48 +0100

    refactor: switch to using bootstrap for styling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// Import Bootstrap JS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
