8be000644f3336c26b108ae1db83b5a588616941
David Zhang <32676710+dzhang32@users.noreply.github.com>
2025-07-30 19:31:20 +0100

    feat: sort recipes alphabetically

--- frontend/src/components/RecipeCards.tsx
+++ frontend/src/components/RecipeCards.tsx
@@ -10,6 +10,9 @@ export default function RecipeCards() {
   useEffect(() => {
     fetch("http://localhost:8000/recipes")
       .then((res) => res.json())
+      .then((res) =>
+        res.sort((a: Recipe, b: Recipe) => a.name.localeCompare(b.name))
+      )
       .then((data: Recipe[]) => {
         setRecipes(data.map(tidyRecipe));
       })
@@ -18,27 +21,29 @@ export default function RecipeCards() {
 
   return (
     <>
-      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 g-4">
-        {recipes?.map((recipe) => (
-          <div className="col" key={recipe.id}>
-            <div className="card h-100">
-              <div className="card-body">
-                <h5 className="card-title">{recipe.name}</h5>
-                <button
-                  type="button"
-                  className="btn btn-primary"
-                  data-bs-toggle="modal"
-                  data-bs-target="#instructionsModal"
-                  onClick={() => setSelectedRecipe(recipe)}
-                >
-                  Instructions
-                </button>
+      <div className="container">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 g-4">
+          {recipes?.map((recipe) => (
+            <div className="col" key={recipe.id}>
+              <div className="card h-100">
+                <div className="card-body">
+                  <h5 className="card-title">{recipe.name}</h5>
+                  <button
+                    type="button"
+                    className="btn btn-primary"
+                    data-bs-toggle="modal"
+                    data-bs-target="#instructionsModal"
+                    onClick={() => setSelectedRecipe(recipe)}
+                  >
+                    Instructions
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <InstructionsModal recipe={selectedRecipe} />
       </div>
-      <InstructionsModal recipe={selectedRecipe} />
     </>
   );
 }
