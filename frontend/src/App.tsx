78e98b074e2f66162b5a85f9246da0cb5bea3eeb
David Zhang <32676710+dzhang32@users.noreply.github.com>
2025-07-30 18:58:00 +0100

    feat: add navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
-import './App.css'
-import RecipeCards from './components/RecipeCards'
-
+import "./App.css";
+import RecipeCards from "./components/RecipeCards";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
     <div>
+      <NavigationBar />
       <RecipeCards />
     </div>
   );
 }
 
-export default App
+export default App;
