19c457df474cc1f9cbc72ae91413ae6b3a127922
David Zhang <32676710+dzhang32@users.noreply.github.com>
2025-09-23 18:40:51 +0100

    feat: add route to obtain ingredients

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -2,14 +2,9 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 
 from app.db import get_session
-from app.models import Recipe
+from app.models import Ingredient, Recipe
 
 router = APIRouter()
-
-
-@router.get("/")
-async def root() -> dict[str, str]:
-    return {"message": "Hello World"}
 
 
 @router.get("/recipes", response_model=list[Recipe])
@@ -31,3 +26,14 @@ async def get_recipe(id: int, session: Session = Depends(get_session)) -> Recipe
         raise HTTPException(status_code=404, detail="Recipe not found")
 
     return recipe
+
+
+@router.get("/recipes/{id}/ingredients", response_model=list[Ingredient])
+async def get_recipe_ingredients(
+    id: int, session: Session = Depends(get_session)
+) -> list[Ingredient]:
+    """
+    Get the ingredients for a recipe by its unique identifier.
+    """
+    recipe = session.get(Recipe, id)
+    return recipe.ingredients
